776f9c19a5ce3dcb028eb160470818ff7960d40f
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-04 12:37:24 +0100

    commit master fait par Tristan

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_154721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
