829f6a27d4137d79f940cb854aa99b2f44500420
Alvaro Castillo <martinvancastle@outlook.com>
2025-03-06 15:22:23 +0100

    new index modifications, bookings showed in card class

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,17 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :deck
 
+validates :date_of_beginning, presence: true
+validates :date_of_end, presence: true
+validate :date_of_end_after_date_of_beginning
+
+private
+
+def date_of_end_after_date_of_beginning
+  return if date_of_end.blank? || date_of_beginning.blank?
+
+  if date_of_end < date_of_beginning
+    errors.add(:date_of_end, "must be after the start date")
+  end
+end
 end
