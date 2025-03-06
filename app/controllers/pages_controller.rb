3566b9fb69d742d95e7bb8104ed623810af91a90
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-03-06 14:28:24 +0100

    Modification de la hompepagev3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
-    @decks = Deck.first(3)
+    decks = Deck.all.sample(50)
+    shuffled_decks = decks.shuffle
+
+    @decks1 = shuffled_decks.first(3)
+    @decks2 = shuffled_decks.last(3)
   end
 end
