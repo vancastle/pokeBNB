b0411e83c12cac053ebd80a15b5f7045fd84bdf3
Erwann Defoy <ecdefoy1@gmail.com>
2025-03-07 11:36:50 +0100

    creation of the partial display

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :decks, only: %i[index show] do
+  resources :decks do
     resources :bookings, only: %i[new create]
   end
 
